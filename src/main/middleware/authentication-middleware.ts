6a0e1959ddaa43f002d153faf8a81677682ecf4a
Dunieski Otano <46684047+dunieskiotano@users.noreply.github.com>
2019-01-29 00:45:45 -0500

    Updated Project

--- src/main/middleware/authentication-middleware.ts
+++ src/main/middleware/authentication-middleware.ts
@@ -1,13 +1,11 @@
 import { RoleDAO } from '../DAOs/roleDAO';
 import session from 'express-session';
 export function authMiddleware(req, res, next) {
-    console.log('AAAAAAAA',req.session.user.role);
-    if (req.session.user.role=== undefined) {
+        if (req.session.user.role === undefined) {
         //CHECKS IF USER IS DEFINED
         res.status(401).send('The incoming token has expired');
     } else {
         const user = req.session.user.role;
-        console.log('AAAAAAAA',req.session.user.role);
         switch (user) {
             case 'admin':
                 next();
@@ -24,9 +22,9 @@ export function authMiddleware(req, res, next) {
     }
 }
 
- //MIDDLEWARE TO GRANT PERMISSIONS TO ADMIN
+//MIDDLEWARE TO GRANT PERMISSIONS TO ADMIN
 export function authAdminMiddleware(req, res, next) {
-    
+
     if (req.session.user.role === "admin") {
         next()
     } else {
@@ -53,7 +51,7 @@ export function authAdminAndFinancialManagerMiddleware(req, res, next) {
 
 //MIDDLEWARE TO GRANT PERMISSIONS TO ASSOCIATE
 export function authAssociateMiddleware(req, res, next) {
-    if (req.session.user.role === req.params.id && req.session.user==='associate') {
+    if (req.session.user.role === req.params.id && req.session.user === 'associate') {
         next()
     } else {
         res.status(401).send('Error message: Access is denied. You may not have the appropriate permissions to perform this operation.');
