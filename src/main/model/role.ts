fda423e1f5bb31140db07cc8a930f1e81fc125a6
Dunieski Otano <46684047+dunieskiotano@users.noreply.github.com>
2019-01-30 01:56:02 -0500

    Project with more updates!

--- src/main/model/role.ts
+++
@@ -1,9 +0,0 @@
-export class Role {
-    roleId: number;
-    role: string;
-
-    constructor(roleId: number = 0, role: string = '') {
-        this.roleId = roleId;
-        this.role = role;
-    }
-}
